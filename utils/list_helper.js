514e58ff7fa6ea2aa2392905e5f8db6be39d423d
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2022-07-20 16:16:08 +0300

    add part 4 exercise 4.4: helper functions and unit tests, step 2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,21 @@
+const logger = require('./logger')
+
+
 const dummy = (blogs) => {
   return 1
+}
+
+const totalLikes = (blogs) => {
+
+  const reducer = (sum, item) => {
+    logger.info('item on ', item)
+    logger.info('item.likes on ', item.likes)
+    return sum + item.likes
+  }
+  logger.info('pituus on ', blogs.length)
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce(reducer, 0)
 }
 
 
@@ -20,7 +36,9 @@ const average = array => {
     : array.reduce(reducer, 0) / array.length
 }
 
+
 module.exports = {
+  totalLikes,
   dummy,
   reverse,
   average
